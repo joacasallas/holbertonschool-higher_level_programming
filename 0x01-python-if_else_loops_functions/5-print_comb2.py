908f67248ca43dd0d7872068fcc4922054e957cd
joacasallas <joacasallas@gmail.com>
2022-01-05 08:19:16 -0500

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def print_num():
     for i in range(99):
-        if i < 10:
-            print("0{}, ".format(i), end="")
-        else:
-            print("{}, ".format(i), end="")
+        print("{:02d}, ".format(i), end="")
 
 
 print_num()
