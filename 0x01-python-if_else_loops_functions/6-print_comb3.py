113f433944b6be7bc9c6705275822586cadb8540
joacasallas <joacasallas@gmail.com>
2022-01-05 14:21:36 -0500

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-list_num = []
-for i in range(10):
+for i in range(9):
     for j in range(10):
-        if i == j:
-            continue
-        num_pair = i, j
-        num_pair_rev = j, i
-        if num_pair_rev in list_num:
-            continue
-        list_num.append(num_pair)
         if i == 8 and j == 9:
             continue
-        print("{}{}, ".format(i, j), end="")
+        if j > i:
+            print("{}{}".format(i, j), end=", ")
 print("{}{}".format(8, 9))
