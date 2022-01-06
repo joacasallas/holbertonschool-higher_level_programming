cacb6c950a7d53ad5f093c2a58d9d2a639b7cdc3
joacasallas <joacasallas@gmail.com>
2022-01-05 23:15:44 -0500

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-lista = []
-lista2 = []
+j = -25
 for i in range(97, 123):
+    i = i - j
+    j = j + 2
     if i % 2 != 0:
         i = i - 32
-    lista.append(chr(i))
-lista2 = ("".join(lista[:: -1]))
-print("{}".format(lista2), end="")
+    print("{}".format(chr(i)), end="")
