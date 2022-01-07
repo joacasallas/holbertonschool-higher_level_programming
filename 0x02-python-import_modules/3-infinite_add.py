aedbb46700be13c1e3628c9c999a88504f6d1abb
joacasallas <joacasallas@gmail.com>
2022-01-07 10:37:30 -0500

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add
     import sys
     number = 0
     for i in range(1, len(sys.argv)):
-        number = add(number, int(sys.argv[i]))
+        number = (number + int(sys.argv[i]))
     print(number)
