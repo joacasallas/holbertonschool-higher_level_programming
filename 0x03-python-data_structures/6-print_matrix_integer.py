36f11d3032e5304224d0ff077b426b0d4ebdf45c
joacasallas <joacasallas@gmail.com>
2022-01-09 17:28:50 -0500

    update

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == matrix[i][-1]:
+                print("{}".format(matrix[i][j]), end="")
+            else:
+                print("{}".format(matrix[i][j]), end=" ")
+        print("")
