a240204ead5e1b02d83ba8ddbd9e012296eb3605
joacasallas <joacasallas@gmail.com>
2022-01-11 17:47:23 -0500

    update

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max = 0
+    if a_dictionary is None:
+        return None
+    else:
+        for key in a_dictionary:
+            value = a_dictionary.get(key)
+            if value > max:
+                max = value
+                keymax = key
+            else:
+                continue
+    return keymax
