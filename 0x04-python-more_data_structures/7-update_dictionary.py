1e4c456ae04090de46dbae993b025ec4e45a9792
joacasallas <joacasallas@gmail.com>
2022-01-11 10:56:50 -0500

    update

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    new_a_directory = {}
+    for key in a_dictionary:
+        print(key+":", a_dictionary[key])
+        new_a_directory.get(key)
+    return(new_a_directory)
