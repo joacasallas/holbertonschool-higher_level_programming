ef6ddaea47c685d8593969cc5045d9cb0c4ad700
joacasallas <joacasallas@gmail.com>
2022-01-11 05:13:18 -0500

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+    new_matrix = []
+    for rows in matrix:
+        new_matrix.append([n**2 for n in rows])
+    return new_matrix
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,3 +1,4 @@
+0x04-python-more_data_structures
 0-square_matrix_simple.py
 1-search_replace.py
 2-uniq_add.py
