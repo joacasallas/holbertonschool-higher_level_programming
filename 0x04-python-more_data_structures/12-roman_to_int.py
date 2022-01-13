d44f5045989da7d2d1ec03577e0205a119f5728a
joacasallas <joacasallas@gmail.com>
2022-01-13 00:25:32 -0500

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,18 +3,28 @@ def roman_to_int(roman_string):
     if len(roman_string) < 1:
         return None
     dictionary = {
-        'I' : 1, 'V' : 5, 'X' : 10, 
-        'L' : 50, 'C' : 100, 'D' : 500, 
-        'M' : 1000
+        'I': 1, 'V': 5, 'X': 10,
+        'L': 50, 'C': 100, 'D': 500,
+        'M': 1000,  'zero': 0
     }
-    number = 0
     last = 2000
-    for n in roman_string:
-        if n in dictionary:
-            if int(dictionary[n]) > last:
-                number = int(dictionary[n]) - number
+    number = 0
+    list_next = []
+    for i in range(1, len(roman_string)):
+        list_next.append(roman_string[i])
+    list_next.append("zero")
+    for i in range(len(roman_string)):
+        if roman_string[i] in dictionary:
+            if dictionary[roman_string[i]] <= last:
+                if dictionary[roman_string[i]] < dictionary[list_next[i]]:
+                    last = dictionary[roman_string[i]]
+                    continue
+                else:
+                    number += dictionary[roman_string[i]]
+                    last = dictionary[roman_string[i]]
             else:
-                number += int(dictionary[n])
-            last = int(dictionary[n])
-            #number += int(dictionary[n])
+                if dictionary[roman_string[i]] > dictionary[list_next[i]]:
+                    previus = dictionary[roman_string[i - 1]]
+                    number += (dictionary[roman_string[i]] - previus)
+                    last = dictionary[roman_string[i]]
     return number
