d706c8e45602f81329d3ffad97b4485e3fb61b24
joacasallas <joacasallas@gmail.com>
2022-01-23 05:15:03 -0500

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square():
+    """write a class that defines a square"""
+    def __init__(self, size=0):
+        self.__size = size
