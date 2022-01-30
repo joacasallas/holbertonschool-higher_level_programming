a5ae7947ecdadab66530df9aba540fc9f7210a24
joacasallas <joacasallas@gmail.com>
2022-01-30 13:01:01 -0500

    update

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,6 +9,3 @@ class TestMaxInteger(unittest.TestCase):
     def test_function(self):
         list = [1, 2, 3, 4]
         self.assertEqual(max_integer(list), 4)
-        self.assertTrue([1, 4, 5], 5)
-        self.assertTrue([5, 4, 1], 5)
-        self.assertTrue([4, 5, 1], 5)
