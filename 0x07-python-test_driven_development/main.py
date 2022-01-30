eb187eca9ef6096add0304edc76b83107bed994d
joacasallas <joacasallas@gmail.com>
2022-01-30 12:02:58 -0500

    update

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -1,3 +1,41 @@
-lista = [1,2,3]
+import unittest
 
-print(type(lista[0]))
+class Mis_tests(unittest.TestCase):
+    DatoUno = 0
+    DatoDos = 0
+    correr = True
+
+    def setUp(self):
+        self.DatoUno = 2
+        self.DatoDos = 2
+        if self.DatoUno * self.DatoDos == 2000:
+            correr = False
+
+    def test_suma(self):
+        self.assertEqual(self.DatoUno+self.DatoDos,4)
+        self.assertTrue(self.correr)
+    
+    def test_resta(self):
+        self.assertTrue(self.DatoUno-self.DatoDos==0)
+        self.assertTrue(self.correr)
+
+    def tearDown(self):
+        print("Borrando datos ...")
+        print("llevando la base de datos a posicion inicial")
+        print("guardando resultado del caso")
+
+    def test_verificar_iguales(self):
+        a = 5
+        b = 4+1
+
+        self.assertEqual(a,b)
+
+    def test_verificar_iguales(self):
+        a = 10
+        b = 5*2
+
+        self.assertNotEqual(a,b, "a tiene que ser igual a b")
+    
+
+if __name__ =='__main__':
+    unittest.main()
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_function(self):
+        self.assertTrue([1, 4, 5], 4)
