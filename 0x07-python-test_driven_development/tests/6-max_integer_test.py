fc1996a4469caeeadfa13b3430647066409ce71a
joacasallas <joacasallas@gmail.com>
2022-01-30 13:08:52 -0500

    update

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,6 +11,10 @@ class TestMaxInteger(unittest.TestCase):
         list2 = [4, 2, 3, 4]
         list3 = [1, 4, 3]
         list4 = [1, -3, 3, 4]
+
+        for i in range(len(list4)):
+            if list4[i] < 0:
+                list4[i] = list4[i] * -1
         self.assertEqual(max_integer(list1), 4)
         self.assertEqual(max_integer(list2), 4)
         self.assertEqual(max_integer(list3), 4)
