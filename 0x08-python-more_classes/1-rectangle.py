df6df6b42e1f165bd69e0d3ec09a8441807f15d1
joacasallas <joacasallas@gmail.com>
2022-01-31 22:09:30 -0500

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,37 +8,27 @@ class Rectangle():
         """Inizialitation object"""
         self.width = width
         self.height = height
-        if type(width) == str:
-            raise TypeError("width must be an integer")
-        if type(height) == str:
-            raise TypeError("height must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if height < 0:
-            raise ValueError("height must be >= 0")
 
+        @property
+        def width(self):
+            return self.__width
 
-@property
-def width(self):
-    return self.__width
+        @width.setter
+        def width(self, value):
+            if type(value) == str:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
 
+        @property
+        def height(self):
+            return self.__height
 
-@width.setter
-def width(self, value):
-    if type(value) == str:
-        raise TypeError("width must be an integer")
-    if value < 0:
-        raise ValueError("width must be >= 0")
-    self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if type(value) == str:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        @height.setter
+        def height(self, value):
+            if type(value) == str:
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
