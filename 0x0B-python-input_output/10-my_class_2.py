8b5965f8c44a840bb255f5fd037ffc111d90686d
joacasallas <joacasallas@gmail.com>
2022-02-01 23:12:08 -0500

    update

--- 0x0B-python-input_output/10-my_class_2.py
+++ 0x0B-python-input_output/10-my_class_2.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""Modulo estudiante"""
+"""Creates a student class"""
 
 
-class Student():
-    """Se crea la clase estudian y se define"""
+class Student ():
+    """Creates a student class"""
 
     def __init__(self, first_name, last_name, age):
-        """Instanciacion"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returna una representacion en diccionario"""
-        diccionario = dict()
+
+        new_dict = {}
+
         if attrs is None:
             return self.__dict__
-        else:
-            for iterador in attrs:
-                if iterador in self.__dict__:
-                    diccionario[iterador] = self.__dict__[iterador]
-            return (diccionario)
+
+        for attr in attrs:
+            if attr in self.__dict__.keys():
+                new_dict[attr] = self.__dict__[attr]
+
+        return new_dict
