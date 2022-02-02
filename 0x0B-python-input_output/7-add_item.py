f6100343200e00109167b31dba3a9337984f8a43
joacasallas <joacasallas@gmail.com>
2022-02-01 23:37:58 -0500

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
-"""access commandline arguments"""
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-"""create object from JSON file"""
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-"""writes an object to text file, using JSON representation"""
+"""
+adds all arguments to a Python list, and then save them to a file
+"""
+import sys
 
-filename = "add_item.json"
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+open("add_item.json", "a")
 try:
-    content = load_from_json_file(filename)
-except:
-    content = []
-
-for i in range(1, len(argv)):
-    content.append(argv[i])
-save_to_json_file(content, filename)
+    l = load_from_json_file("add_item.json")
+except ValueError:
+    l = []
+save_to_json_file(l + sys.argv[1:], "add_item.json")
