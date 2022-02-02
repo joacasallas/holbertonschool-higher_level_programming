fb0a6d9d7a604d7a1610fbeb3f59977584d385d4
joacasallas <joacasallas@gmail.com>
2022-02-01 23:25:12 -0500

    update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,30 +1,51 @@
 #!/usr/bin/python3
+
 import sys
-import io
+from signal import signal, SIGINT
 
-#input = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
-#with open(input, "r", encoding="utf-8") as file:
- #   for line in file:
-  #      print(line)
 
-#input = io.TextIOWrapper(sys.stdin , encoding='utf-8')
+count, file_size = 0, 0
+allowed_codes = {
+    '200': 0, '301': 0, '400': 0, '401': 0,
+    '403': 0, '404': 0, '405': 0, '500': 0
+}
 
-dictstatus = {}
-totalsize = 0
-totalcount = 0
+
+def ten_times():
+
+    print("File size: {:d}".format(file_size))
+
+    for key in sorted(allowed_codes.keys()):
+        val = allowed_codes.get(key)
+        if val != 0:
+            print("{}: {:d}".format(key, val))
+
+
+def handler(signal_received, frame):
+    ten_times()
+    sys.exit(0)
+
 for line in sys.stdin:
-    split = line.split()
-    status = split[-2]
-    totalsize += int(split[-1])
-    if status in dictstatus.keys():
-        dictstatus[status] += 1
-    else:
-        dictstatus[status] = 1
-    totalcount += 1
-    if totalcount == 10:
-        sortme = sorted(dictstatus.keys())
-        print("File size:", totalsize)
-        for keys in sortme:
-            print("{}: {}".format(keys, dictstatus[keys]))
-        totalcount = 0
-        continue
+
+    signal(SIGINT, handler)
+
+    if count == 10:
+        ten_times()
+        count = 0
+
+    split_line = line.split()
+    try:
+        if split_line[-2] and split_line[-2] in allowed_codes.keys():
+            allowed_codes[split_line[-2]] += 1
+    except:
+        pass
+
+    try:
+        size = int(split_line[-1])
+        file_size += size
+    except:
+        pass
+
+    count += 1
+
+ten_times()
