6006af7a2608698cf7314381d05704bee74ab78f
joacasallas <joacasallas@gmail.com>
2022-02-01 23:27:21 -0500

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+""" append_after module """
 
 
 def append_after(filename="", search_string="", new_string=""):
-
-    with open(filename, 'r', encoding='UTF-8') as f:
-        lines = f.readlines()
-
-    with open(filename, 'w', encoding='UTF-8') as f:
-        for line in lines:
+    """ append a line just after the search string was founded """
+    with open(filename, mode="r", encoding="UTF8") as f:
+        new_line = ""
+        for line in f:
             if search_string in line:
-                line = line + new_string
-            f.write(line)
+                new_line += line + new_string
+            else:
+                new_line += line
+    with open(filename, mode="w", encoding="UTF8") as f:
+        f.write(new_line)
