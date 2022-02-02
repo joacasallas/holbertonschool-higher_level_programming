ccce55958c26d578f7b23440f8c457f3baff0ac3
joacasallas <joacasallas@gmail.com>
2022-02-01 22:49:41 -0500

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+"""Function that reads a text file (UTF8) and prints it to stdout"""
+
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """Print the contents of a UTF8 text file to stdout"""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
