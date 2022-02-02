5bc6fb9e59b1dd6a9092972cc695620c0a8233c6
joacasallas <joacasallas@gmail.com>
2022-02-01 23:35:53 -0500

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python3
-""" append_after module """
+"""
+reads stdin line by line and computes metrics
+"""
+import sys
 
+file_size = 0
+status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+i = 0
+try:
+    for line in sys.stdin:
+        tokens = line.split()
+        if len(tokens) >= 2:
+            a = i
+            if tokens[-2] in status_tally:
+                status_tally[tokens[-2]] += 1
+                i += 1
+            try:
+                file_size += int(tokens[-1])
+                if a == i:
+                    i += 1
+            except:
+                if a == i:
+                    continue
+        if i % 10 == 0:
+            print("File size: {:d}".format(file_size))
+            for key, value in sorted(status_tally.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
 
-def append_after(filename="", search_string="", new_string=""):
-    """ append a line just after the search string was founded """
-    with open(filename, mode="r", encoding="UTF8") as f:
-        new_line = ""
-        for line in f:
-            if search_string in line:
-                new_line += line + new_string
-            else:
-                new_line += line
-    with open(filename, mode="w", encoding="UTF8") as f:
-        f.write(new_line)
+except KeyboardInterrupt:
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
