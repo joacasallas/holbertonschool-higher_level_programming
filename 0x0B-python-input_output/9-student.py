4b7ad80b13f9a79689c4e50ff8abf2232661e29a
joacasallas <joacasallas@gmail.com>
2022-02-01 23:07:04 -0500

    update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""method for student creation"""
+"""Modulo estudiante"""
 
 
-class Student:
-    """Student obj"""
+class Student():
+    """Se crea la clase estudian y se define"""
 
     def __init__(self, first_name, last_name, age):
+        """Instanciacion"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """Returna una representacion en diccionario"""
         return self.__dict__
