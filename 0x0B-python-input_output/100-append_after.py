81d7ee664df48526c09478b894f419803b6963fa
joacasallas <joacasallas@gmail.com>
2022-02-01 23:26:30 -0500

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""Module containing function that modifies file"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Inserts `new_string` into `filename` after lines containing
-         `search_string`.
-    Args:
-        filename (str): file to modify
-        search_string (str): string to look for in `filename`
-        new_string (str): string to insert after `search_string`
-    """
-    with open(filename, 'r+') as f:
-        lines = [line for line in f]
-        f.seek(0)
-        for idx, line in enumerate(lines):
+
+    with open(filename, 'r', encoding='UTF-8') as f:
+        lines = f.readlines()
+
+    with open(filename, 'w', encoding='UTF-8') as f:
+        for line in lines:
             if search_string in line:
-                lines.insert(idx + 1, new_string)
-        f.writelines(lines)
+                line = line + new_string
+            f.write(line)
