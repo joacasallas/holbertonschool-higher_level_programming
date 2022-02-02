8772f98ab02e554d3fc56082bffceacdc1c2da08
joacasallas <joacasallas@gmail.com>
2022-02-01 22:57:34 -0500

    update

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
-
-
+#!/usr/bin/python3#
+""" function that returns returns an object  represented by a JSON string """
 import json
 
 
 def from_json_string(my_str):
+    """ saving struct data whit json """
     return json.loads(my_str)
