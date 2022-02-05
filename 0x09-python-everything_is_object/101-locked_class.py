e3ac01c75fcd5fa7ea64ea02bfca8ece7a5d06d6
joacasallas <joacasallas@gmail.com>
2022-02-05 10:39:17 -0500

    update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""A locked class that only lets the user dynamically create the instance
+    attribute 'first_name'"""
+
+
+
 class LockedClass:
     """A locked class that only lets the user dynamically create the instance
     attribute 'first_name'"""
     __slots__ = ['first_name']
-     """A locked class that only lets the user dynamically create the instance
-    attribute 'first_name'"""
