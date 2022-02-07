27b0fe4de758323e84056956475a3390531a39f9
joacasallas <joacasallas@gmail.com>
2022-02-06 23:17:46 -0500

    update

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,6 +3,7 @@
 
 
 class BaseGeometry():
+    """New Class"""
     def area(self):
         """raises an Exception with the message area() is not implemented"""
         raise Exception("area() is not implemented")
