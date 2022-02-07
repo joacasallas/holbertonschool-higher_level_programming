d75feacc3a2d85d6a2f29b70633c6261c86aa0f9
joacasallas <joacasallas@gmail.com>
2022-02-07 16:57:56 -0500

    update

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,7 +9,12 @@ class Rectangle(BaseGeometry):
     """ class Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
         """ Initialization"""
+        if height == 0:
+            raise AttributeError("'Rectangle' object has no attribute 'height'")
+        if width == 0:
+             raise AttributeError("'Rectangle' object has no attribute 'width'")
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self._width = width
         self. height = height
+        
