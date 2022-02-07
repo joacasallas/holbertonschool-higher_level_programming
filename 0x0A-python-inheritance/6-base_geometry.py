f3b2ea4d1f86b742ec7dbea1a5def42bc5a3f79e
joacasallas <joacasallas@gmail.com>
2022-02-06 22:49:49 -0500

    update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry"""
+
+
+class BaseGeometry():
+    """Write a class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
