daf4f3236338847347b72441b4d0a1fbe8abc705
joacasallas <joacasallas@gmail.com>
2022-02-08 13:12:27 -0500

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,12 @@
 
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, "r") as file:
-        for line in file:
-            print(line, end="")
-        print()
+    try:
+        with open(filename, "r") as file:
+            for line in file:
+                print(line, end="")
+            print()
+            file.close()
+    except FileNotFoundError:
+        print("[Errno 2] No such file or directory: '{}'".format(filename))
+        exit()
