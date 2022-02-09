310ffc90344b41f594ef9cda49bf2ff1abc25a48
joacasallas <joacasallas@gmail.com>
2022-02-08 21:36:41 -0500

    update

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a script that adds all arguments to a Python list,
+and then save them to a file:"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if __name__ == "__main__":
+    try:
+        validation = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        validation = []
+    validation.extend(sys.argv[1:])
+    save_to_json_file(validation, "add_item.json")
