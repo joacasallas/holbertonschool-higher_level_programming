5782ad0d997392070bfab3189440c534ba99f775
joacasallas <joacasallas@gmail.com>
2022-02-08 21:40:47 -0500

    update

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """function that writes an Object to a text file,
     using a JSON representatio"""
-    with open(filename, "a") as file:
-        json.dump(my_obj, file)
+    with open(filename, "w") as file:
+        return json.dump(my_obj, file)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Write a script that adds all arguments to a Python list,
-and then save them to a file:"""
+"""
+Importing.
+"""
 import sys
 
 
