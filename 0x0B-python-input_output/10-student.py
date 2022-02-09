b2b76714514e43358aaa9205faf3412a1545fe61
joacasallas <joacasallas@gmail.com>
2022-02-08 22:44:17 -0500

    update

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
-"""Write a class Student that defines a student"""
-Student = __import__('9-student').Student
+"""
+Definning a class Student.
+"""
 
 
-class Student():
-    """Write a class Student that defines a student"""
+class Student:
+    """
+    Student representation.
+    """
+
     def __init__(self, first_name, last_name, age):
+        """
+        Initializing student class.
+        Arguments:
+            first_name (str): Student's first name.
+            last_name  (str): Student's Last name.
+            age       (int): Student's age.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student instance"""
+        """
+        dictionary Student's representation.
+        """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
