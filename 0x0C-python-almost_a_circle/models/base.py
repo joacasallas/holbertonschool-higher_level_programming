cc6825cdc82249031fd04a9ca2d3b2cd06070238
joacasallas <joacasallas@gmail.com>
2022-02-10 15:30:28 -0500

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,12 +2,24 @@
 """first class"""
 
 
+import json
+
+
 class Base():
     """first class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
