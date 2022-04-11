757fa84f681a473a19a196d0a03eba02c6556bee
joacasallas <joacasallas@gmail.com>
2022-04-11 17:01:56 -0500

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-a = 'C is fun';
-b = 'Python is cool';
-c = 'JavaScript is amazing';
+let a = 'C is fun';
+let b = 'Python is cool';
+let c = 'JavaScript is amazing';
 console.log(`${a}\n${b}\n${c}`);
