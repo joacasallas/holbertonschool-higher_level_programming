816fef201e0fe74cbac73650f83d5912fe4200d9
joacasallas <joacasallas@gmail.com>
2022-04-11 23:37:55 -0500

    update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
+if (isNaN(Number(process.argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${Number(process.argv[2])}`);
+  console.log(`My number: ${process.argv[2]}`);
 }
