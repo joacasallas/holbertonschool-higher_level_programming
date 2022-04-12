a09b9a1df7970ff156023afcec8818ea42193729
joacasallas <joacasallas@gmail.com>
2022-04-12 12:13:53 -0500

    update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 //  computes and prints a factorial
 function factorial (n) {
+  if (isNaN(Number(process.argv[2]))) { 
+      return 1;
+  }
   if (n > 1) {
     n = n * factorial(n - 1);
   }
