fa83f31e0bb17ab83d15f58eb1bd3c40f5c3e056
joacasallas <joacasallas@gmail.com>
2022-04-12 23:08:36 -0500

    update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,27 @@
 #!/usr/bin/node
-// script that gets the contents of a webpage and stores it in a file.
-
-const url = process.argv[2];
-const file = process.argv[3];
-const req = require('request');
-const fileStream = require('fs');
-
-req(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    fileStream.writeFile(file, body, 'utf-8', (error) => {
-      if (error) console.log(error);
-    });
-  }
-});
+const request = require('request');
+const film = process.argv[2];
+let url = 'http://swapi.co/api/people/';
+function filmcharacters (film, url) {
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      let jsonobj = JSON.parse(body);
+      let people = jsonobj.results;
+      for (let i in people) {
+        for (let j in people[i].films) {
+          if (people[i].films[j].includes(film)) {
+            console.log(people[i].name);
+          }
+        }
+      }
+      if (jsonobj.next !== null) {
+        filmcharacters(film, jsonobj.next);
+      }
+    } else {
+      console.log('An error occured. Status code: ' + response.statusCode);
+    }
+  });
+}
+filmcharacters(film, url);
