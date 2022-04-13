7daec12dd3e77f887b8419b3490e261ff7f853be
joacasallas <joacasallas@gmail.com>
2022-04-12 23:12:07 -0500

    update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,27 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const film = process.argv[2];
-let url = 'http://swapi.co/api/people/';
-function filmcharacters (film, url) {
-  request(url, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else if (response.statusCode === 200) {
-      let jsonobj = JSON.parse(body);
-      let people = jsonobj.results;
-      for (let i in people) {
-        for (let j in people[i].films) {
-          if (people[i].films[j].includes(film)) {
-            console.log(people[i].name);
-          }
-        }
+
+const films = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(films, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const film = JSON.parse(body);
+  for (const charAPI of film.characters) {
+    request(charAPI, (err, res, body) => {
+      if (err) {
+        console.error(err);
       }
-      if (jsonobj.next !== null) {
-        filmcharacters(film, jsonobj.next);
-      }
-    } else {
-      console.log('An error occured. Status code: ' + response.statusCode);
-    }
-  });
-}
-filmcharacters(film, url);
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  }
+});
