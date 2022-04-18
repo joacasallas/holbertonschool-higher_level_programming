f2a819f393ac01dd7d78f95e3eb11e35920ed525
joacasallas <joacasallas@gmail.com>
2022-04-18 03:51:01 -0500

    update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // import an array and compute a new array.
-let listCopy = require('./100-data').list;
+const listCopy = require('./100-data').list;
 console.log(listCopy);
 console.log(listCopy.map((x, y) => x * y));
