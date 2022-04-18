b3c8556d74218a876e19e135e1ad4d4136293f30
joacasallas <joacasallas@gmail.com>
2022-04-18 04:09:10 -0500

    update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // import an array and compute a new array.
-const listCopy = require('./100-data').list;
-console.log(listCopy);
-console.log(listCopy.map((x, y) => x * y));
+const list = require('./100-data').list;
+console.log(list);
+console.log(list.map((x, y) => x * y));
---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// imports a dictionary of occurrences by user id
+const dict = require('./101-data').dict;
+const n = {};
+for (const i in dict) {
+  if (n[dict[i]] === undefined) {
+    n[dict[i]] = [];
+  }
+  n[dict[i]].push(i);
+}
+console.log(n);
