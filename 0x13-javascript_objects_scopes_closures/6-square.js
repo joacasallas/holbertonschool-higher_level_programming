0f48f178bc611a9f29931e33dff556ad6ca058e8
joacasallas <joacasallas@gmail.com>
2022-04-18 01:54:00 -0500

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,17 @@
 #!/usr/bin/node
-
-const Rectangle = require('./5-square');
-
 // Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends require('./5-square') {
   constructor (size) {
     super(size, size);
     this.size = size;
   }
 
   charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
     for (let i = 0; i < this.size; i++) {
-      if (c === undefined) {
-        console.log('X'.repeat(this.size));
-      } else {
-        console.log(c.repeat(this.size));
-      }
+      console.log(c.repeat(this.size));
     }
   }
 };
