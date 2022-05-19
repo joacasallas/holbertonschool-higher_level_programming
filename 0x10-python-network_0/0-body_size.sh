2d44767a51cc25ede9879c28e7bdd8ca55fa460f
joacasallas <joacasallas@gmail.com>
2022-05-19 11:09:08 -0500

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a request to that URL
-curl -sI GET "$1" | grep content-length | cut -d" " -f2
+curl -sI GET "$1" | grep -i "Content-length" | cut -d" " -f2
